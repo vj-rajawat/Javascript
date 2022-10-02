9a2f53a761fe2490b07e463cb63a59185201e0b9
vj-rajawat <vijayrajawat01@gmail.com>
2022-10-02 10:11:45 +0530

    reduce function example from galaxy

--- PrintStarPattern.js
+++ PrintStarPattern.js
@@ -1,41 +1,104 @@
 let n = 5;
 let string = "";
+
 // for (let i = 1; i <= n; i++) {
-  // for (let j = 0; j < i; j++) {
-    // string += "*";
-  // }
-  // string += "\n";
+// for (let j = 0; j < i; j++) {
+// string += "*";
 // }
+// string += "\n";
+// }
+
+
 // for (let i = 1; i <= n - 1; i++) {
-  // for (let j = 0; j < n - i; j++) {
-    // string += "*";
-  // }
-  // string += "\n";
+// for (let j = 0; j < n - i; j++) {
+// string += "*";
+// }
+// string += "\n";
 // }
 // console.log(string);
+
+
 // for(let i = 1; i <= n; i++){
-  // for(let j = 1; j <= i; j++){
-    // string += "*";
-  // }
-  // string += "\n";
+// for(let j = 1; j <= i; j++){
+// string += "* ";
 // }
-// console.log(string)
-// 
+// string += "\n";
+// }
+// console.log(string);
+
+
 // let a = 0;
 // for(let i = 1; i <= n; i++){
-  // for(let j = 1; j <= i; j++){
-    // string += a + j;
-  // }
-  // string += "\n";
+// for(let j = 1; j <= i; j++){
+// string += a + j;
+// }
+// string += "\n";
 // }
 // console.log(string)
 
-for(let i = 0; i < n; i++){
-  for(let j = 0; j <= i; j++){
-    let space = n - j;
-    for(let s = 0; s < space; s++)
-string += "";
+
+// for(let i = 0; i <=  2*n-1; i++){
+// let cols = i > n ? 2*n-i : i;
+// for(let j = 0; j < cols; j++) {
+// 
+// string += "* ";
+// }
+// string += "\n";
+// }
+// console.log(string);
+
+
+
+// for(let i = 0; i <= 2*n-1; i++){
+// let cols = i > n ? 2*n-i : i;
+// let space = n-cols;
+// for(let s = 0; s < space; s++){
+// string += " ";
+// }
+// for(let j = 0; j < cols; j++) {
+// 
+// string += "* ";
+// }
+// string += "\n";
+// }
+// console.log(string);
+
+// Square Pattern
+// for(let i = 0; i < n; i++){
+// for(let j = 0; j < n; j++){
+// string += "* ";
+// }
+// string += "\n";
+// }
+// console.log(string);
+
+// Hollow square
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if(i === 0 || i === n-1){
+      string += "*";
+    }else if(j === 0 || j === n-1){
+string += "*";
+    }else{
+      string += " ";
+    }
   }
-string += "* ";
-}
+  string += "\n";
+  }
 console.log(string);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ map_filter-reduce(array).js
@@ -0,0 +1,17 @@
+//Reduce function example
+// function changCar(event) {
+    // let carId = event.target.value;
+    // let slug = car.slug;
+    // console.log(carId);
+    // fetch('http://galaxy.test/api/car/' + carId)
+        // .then(response => response.json())
+        // .then(car => {
+            // selectedCar = car;
+            // console.log(car)
+            // let str = car.variants.reduce((innerHTML, variant) => innerHTML +
+                // `<option value='${variant.id}'>${variant.name}</option>`, '')
+            // let str = "<option value='abc'>abc</option>";
+            // console.log(str)
+
+            // document.getElementById('image').src = car.media[0].original_url;
+            // document.getElementById('car_variant').innerHTML = str;
