ef0b6ef3f592746ac02c8ecacafca9a7314e3261
vj_rajawat <vijayrajawat01@gmail.com>
2024-03-31 18:30:18 +0530

    chapter 1

---
+++ chapter1_practiceSet.js
@@ -0,0 +1,29 @@
+// Chapter 1
+// Q1
+let string = "rajawat" + 10;
+console.log(string);
+
+// Q2
+console.log(typeof(string));
+
+// Q3
+const a1 = {
+    name: "vijay",
+    course: "mca",
+    roll: 21
+}
+console.log(a1['roll']);
+
+// Q4 Add key value in constant object
+a1['student'] = 43;
+a1['friends'] = "rahul";
+console.log(a1);
+
+
+// Q5
+let dictionary = {
+    "application" : "uses",
+    "characteristic" : "khasiyat",
+    "significance" : "types"
+}
+console.log(dictionary.application);
