6b29e5ef08b6c1447d4834672e07a63e0bfc0c60
vj_rajawat <vijayrajawat01@gmail.com>
2024-04-07 18:02:36 +0530

    chapter4 practice set

---
+++ chapter4_practiceSet.js
@@ -0,0 +1,15 @@
+//Q1 find output
+console.log(" har\"".length);
+
+//Q4 Extract word from sentence
+let string = "Please give Rs 1000"
+let rs = string.slice("Please give Rs ".length);
+let int = Number.parseInt(rs);
+// console.log(string.slice("Please give Rs ".length));
+console.log(int);
+
+//Q5 Change character of string is it possible?
+let name = "Vijay";
+name[2] = "a";
+console.log(name.replace())
+console.log(name); // We can't able to change string char
